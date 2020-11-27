11e82ae20251a254a4ef545c80cca1ac4d1f0454
Moses Kamara <kamara.moses@yahoo.com>
2020-11-26 18:44:55 -0800

    created resultlist component to hold the result of the search

---
+++ client/src/components/ResultList/ResultList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class ResultList extends Component {
+
+
+    render() {
+        console.log("Props", this.props.bookState)
+        return (
+            this.props.bookState.map((book) => (
+            <ResultListItem 
+            key={book.id}
+            id={book.id}
+            title={book.volumeInfo.title}
+            link={book.volumeInfo.previewLink}
+            authors={book.volumeInfo.authors && book.volumeInfo.authors.length > 1 ? book.volumeInfo.authors.join(", ") : book.volumeInfo.authors}
+            image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
+            description={book.volumeInfo.description}
+            saveGoogleBook={this.props.saveGoogleBook}
+            />
+        ))
+        )
+                           
+    }
+}
+
+export default ResultList;
