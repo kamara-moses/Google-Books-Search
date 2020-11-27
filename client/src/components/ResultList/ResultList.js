40500c972913cc903877afb9a91df747f9c50945
Moses Kamara <kamara.moses@yahoo.com>
2020-11-26 18:49:45 -0800

    created resultlistitem component that is imported inside resultlist component

--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ResultListItem from '../ResultListItem/ResultListItem';
 
 class ResultList extends Component {
 
---
+++ client/src/components/ResultListItem/ResultListItem.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+
+class ResultListItem extends Component {
+  state = {
+    mounted: false,
+    bgColor: "",
+    color: "",
+    text: "Save",
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      mounted: true,
+    });
+    console.log("Mounted!");
+  };
+
+  getStyle = () => {
+    if (this.state.text === "Save") {
+      this.setState({
+        bgColor: "#00E000",
+        color: "white",
+        text: "Saved",
+      });
+    } else {
+      this.setState({
+        bgColor: "",
+        color: "",
+        text: "Save",
+      });
+    }
+  };
+
+  onClickFunc = () => {
+    this.props.saveGoogleBook(this.props); //.bind(this, this.props);
+    this.getStyle();
+  };
+  render() {
+    const { book } = this.props;
+
+    return (
+      <div>
+        <div className="card">
+          <div className="card-header"></div>
+          <div className="card-body">
+            <img
+              src={this.props.image}
+              style={{ maxWidth: "100px" }}
+              alt="book"
+            />
+            <h5 className="card-title" style={{ margin: "10px 0" }}>
+              {this.props.title}
+            </h5>
+            <p className="card-text">{this.props.description}</p>
+            <p style={{ fontStyle: "italic" }}>
+              Author(s): {this.props.authors}
+            </p>
+            <a
+              href={this.props.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+              style={{ marginRight: "6px" }}
+            >
+              View Book
+            </a>
+            <button
+              onClick={this.onClickFunc}
+              style={{
+                backgroundColor: this.state.bgColor,
+                color: this.state.color,
+              }}
+              className="btn"
+            >
+              {this.state.text}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResultListItem;
