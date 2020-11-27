3fec351de5bfa2fcc38714917f72800472357dc3
Moses Kamara <kamara.moses@yahoo.com>
2020-11-27 11:22:45 -0800

    created the SavedList component for our saved part of the application

---
+++ client/src/components/SavedList/SavedList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class ResultList extends Component {
+
+
+    render() {
+        console.log("Props", this.props.bookState)
+        return (
+            
+        )               
+    }
+}
+
+export default ResultList;
