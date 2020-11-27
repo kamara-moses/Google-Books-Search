3172f0e19bf08624c7cff297b4613e03494bea65
Moses Kamara <kamara.moses@yahoo.com>
2020-11-27 12:22:46 -0800

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Saved from "./pages/Saved";
 import "./App.css";
 
 
