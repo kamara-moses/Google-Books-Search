297b1d2c7c2c66de05acd1f2d2f15bcfaa6fe125
Moses Kamara <kamara.moses@yahoo.com>
2020-11-27 11:26:03 -0800

    imported the savelistitem into the savedlist component. This renders the information in a styled manned onto the saved page

--- client/src/components/SavedList/SavedList.js
+++ client/src/components/SavedList/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import SavedListItem from "../SavedListItem/SavedListItem";
 
 class ResultList extends Component {
 
@@ -7,8 +7,20 @@ class ResultList extends Component {
     render() {
         console.log("Props", this.props.bookState)
         return (
-            
-        )               
+            this.props.bookState.map((book) => (
+            <SavedListItem 
+            key={book._id}
+            id={book._id}
+            title={book.title}
+            link={book.link}
+            authors={book.authors && book.authors.length > 1 ? book.authors.join(", ") : book.authors}
+            image={book.image ? book.image : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
+            description={book.description}
+            deleteGoogleBook={this.props.deleteGoogleBook}
+            />
+        ))
+        )
+                           
     }
 }
 
