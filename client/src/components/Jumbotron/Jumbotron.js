16efa0beaa5650fde1d951ecc35a4fa0794df392
Moses Kamara <kamara.moses@yahoo.com>
2020-11-26 17:35:41 -0800

    created the jombotron component

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center myText">Google Books Search</h1>
+                <br></br>
+                <h2 className="text-center myText">Search and Save Books of interest.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
